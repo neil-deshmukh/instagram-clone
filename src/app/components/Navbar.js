23589d2908381f3576050a3bf141433b37842f16
neil-deshmukh <116821804+neil-deshmukh@users.noreply.github.com>
2024-07-17 18:30:49 +0530

    Added Completed Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,44 @@
+"use client"
+
+import Image from "next/image";
+import logo from "../../../public/instagram-word-logo.png"
+import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  )
+    <header className="flex justify-around shadow-md py-4 items-center">
+      <Image src={logo} width={160} />
+      <ul className="flex">
+        <Link
+          href="/Signup"
+          className={clsx(
+            "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+            { "font-extrabold text-xl": usePathname() == "/Signup" }
+          )}
+        >
+          Sign Up
+        </Link>
+        <Link
+          href="/Signin"
+          className={clsx(
+            "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+            { "font-extrabold text-xl": usePathname() == "/Signin" }
+          )}
+        >
+          Sign In
+        </Link>
+        <Link
+          href="/Profile"
+          className={clsx(
+            "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+            { "font-extrabold text-xl": usePathname() == "/Profile" }
+          )}
+        >
+          Profile
+        </Link>
+      </ul>
+    </header>
+  );
 }
