b453e5b26804b702897fb5d103c67c7c6f74d41a
neil-deshmukh <116821804+neil-deshmukh@users.noreply.github.com>
2024-09-01 18:10:14 +0530

    last commit. yay

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -21,92 +21,86 @@ export default function Navbar() {
       setTokenExists(true)
     }
   }, [])
-  const renderLinks = () => {
-    if (!tokenExists) {
-      return (
-        <>
-          <Link
-            href="/Signup"
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/Signup" }
-            )}
-          >
-            Sign Up
-          </Link>
-          <Link
-            href="/Signin"
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/Signin" }
-            )}
-          >
-            Sign In
-          </Link>
-        </>
-      );
-    } else if (tokenExists) {
-      return (
-        <>
-          <Link
-            href="/"
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/" }
-            )}
-          >
-            Home
-          </Link>
-          <Link
-            href="/Followingposts"
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/Followingposts" }
-            )}
-          >
-            Following Posts
-          </Link>
-          <Link
-            href="/Profile"
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/Profile" }
-            )}
-          >
-            Profile
-          </Link>
-          <Link
-            href="/Createpost"
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/Createpost" }
-            )}
-          >
-            Create Post
-          </Link>
-          <Link
-            href={""}
-            className={clsx(
-              "mx-[15px] cursor-pointer hover:font-semibold text-lg",
-              { "font-extrabold text-xl": usePathname() == "/Createpost" }
-            )}
-          >
-            <button
-              className="cursor-pointer font-bold py-[9px] px-[25px] text-[0.8rem] border-none outline-none text-white bg-[#db183c] rounded-2xl hover:-translate-y-1 hover:shadow-2xl transition-all"
-              onClick={() => (setIsModal ? setIsModal(true) : 1 + 1)}
-            >
-              Log Out
-            </button>
-          </Link>
-        </>
-      );
-    }
-  }
   return (
     <header className="flex justify-around shadow-md py-4 items-center">
       <Image src={logo} width={160} />
       <ul className="flex items-baseline">
-        {renderLinks()}
+        {!tokenExists && (
+          <>
+            <Link
+              href="/Signup"
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/Signup" }
+              )}
+            >
+              Sign Up
+            </Link>
+            <Link
+              href="/Signin"
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/Signin" }
+              )}
+            >
+              Sign In
+            </Link>
+          </>
+        )}
+        {tokenExists && (
+          <>
+            <Link
+              href="/"
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/" }
+              )}
+            >
+              Home
+            </Link>
+            <Link
+              href="/Followingposts"
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/Followingposts" }
+              )}
+            >
+              Following Posts
+            </Link>
+            <Link
+              href="/Profile"
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/Profile" }
+              )}
+            >
+              Profile
+            </Link>
+            <Link
+              href="/Createpost"
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/Createpost" }
+              )}
+            >
+              Create Post
+            </Link>
+            <Link
+              href={""}
+              className={clsx(
+                "mx-[15px] cursor-pointer hover:font-semibold text-lg",
+                { "font-extrabold text-xl": usePathname() == "/Createpost" }
+              )}
+            >
+              <button
+                className="cursor-pointer font-bold py-[9px] px-[25px] text-[0.8rem] border-none outline-none text-white bg-[#db183c] rounded-2xl hover:-translate-y-1 hover:shadow-2xl transition-all"
+                onClick={() => (setIsModal ? setIsModal(true) : 1 + 1)}
+              >
+                Log Out
+              </button>
+            </Link>
+          </>
+        )}
       </ul>
     </header>
   );
